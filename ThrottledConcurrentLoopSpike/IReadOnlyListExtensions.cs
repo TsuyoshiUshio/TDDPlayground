988bef9cb01d156c64be10bb18f71b61d1e5cc78
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-03 13:39:48 +0900

    Implement retrun values

--- ThrottledConcurrentLoopSpike/IReadOnlyListExtensions.cs
+++ ThrottledConcurrentLoopSpike/IReadOnlyListExtensions.cs
@@ -36,6 +36,34 @@ namespace ThrottledConcurrentLoopSpike
             }
         }
 
+        public static async Task<IEnumerable<R>> ParallelForEachAsync<T,R>(this IReadOnlyList<T> items, int maxConcurrency, Func<T, Task<R>> action)
+        {
+            using (var semaphore = new SemaphoreSlim(maxConcurrency))
+            {
+                var tasks = new Task<R>[items.Count];
+                for (int i = 0; i < items.Count; i++)
+                {
+                    tasks[i] = InvokeThrottledAction<T,R>(items[i], action, semaphore);
+                }
+
+                return await Task.WhenAll<R>(tasks.AsEnumerable());
+            }
+        }
+
+        static async Task<R> InvokeThrottledAction<T, R>(T item, Func<T, Task<R>> action, SemaphoreSlim semaphore)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                return await action(item);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+
         public static async Task IndexedParallelForEachAsync<T>(this IReadOnlyList<T> items, int maxConcurrency, Func<T, int, Task> action)
         {
             using (var semaphore = new SemaphoreSlim(maxConcurrency))
--- ThrottledConcurrentLoopSpike/Program.cs
+++ ThrottledConcurrentLoopSpike/Program.cs
@@ -25,32 +25,29 @@ namespace ThrottledConcurrentLoopSpike
 
             IReadOnlyList<int> col = new ReadOnlyCollection<int>(list);
 
-            var dictionary = new ConcurrentDictionary<int, ValueTuple<int,DateTime>>();
-
             // 実行順序は保証できない
-            await col.IndexedParallelForEachAsync(10, async (x, index) =>
+            var artifacts = await col.ParallelForEachAsync<int, ValueTuple<int, DateTime>>(10, async (x) =>
             {
 
-                Console.WriteLine($"{index}: Accepted.");
+                Console.WriteLine($"{x}: Accepted.");
                 await Task.Delay(TimeSpan.FromSeconds(5));
 
                 // 成果物があれば、ディクショナリに詰める
                 var dateTime = DateTime.Now;
                 ValueTuple<int, DateTime> artifact = (x, dateTime);
-                dictionary.TryAdd(index, artifact);
 
-                Console.WriteLine($"{index}: Done by {artifact.Item2}");
+                Console.WriteLine($"{x}: Done by {artifact.Item2}");
+                return artifact;
                 
             });
+
 
             Console.WriteLine("---ordered");
 
             // 順番は保証されている。
-            foreach ( var elm  in col.Select((item, index) => new { item, index }))
+            foreach ( var elm  in artifacts)
             {
-                ValueTuple<int, DateTime> result = default(ValueTuple<int, DateTime>);
-                dictionary.TryGetValue(elm.index, out result);
-                Console.WriteLine($"index : {elm.index} x: {elm.item} time: {result.Item2} ");
+                Console.WriteLine($"x: {elm.Item1} time: {elm.Item2} ");
             }
         }
     }
